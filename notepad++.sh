990e626984ce81167bf908cc9067fe4323b3e4a7
Llawliet0872 <arnav0872@gmail.com>
2020-12-11 23:44:10 +0530

    Notepad++(WINE)

--- notepad++.sh
+++ notepad++.sh
@@ -1,5 +1,5 @@
-curl https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe --output notepad-installer.exe
+wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe
 sudo apt install wine-stable
-wine notepad-installer.exe
-rm notepad-installer.exe
+wine npp.7.9.1.Installer.exe
+rm npp7.9.1.Installer.exe
 
