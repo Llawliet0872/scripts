f0074740fd191bf9b6a647d28deaf4bf675bfb13
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 21:17:47 +0530

    Scripts for litepack

--- matcha-themes.sh
+++ matcha-themes.sh
@@ -2,6 +2,6 @@ echo Installing Dependencies
 sudo apt-get install gtk2-engines-murrine gtk2-engines-pixbuf git
 git clone https://github.com/vinceliuice/Matcha-gtk-theme.git
 cd matcha
-sudo ./install.sh
+./install.sh
 cd ..
 rm -rf matcha
