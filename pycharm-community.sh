9c52629b6ebd02dd2c368539f181f90416989ae5
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 12:58:35 +0530

    Scripts for litepack

---
+++ pycharm-community.sh
@@ -0,0 +1,11 @@
+wget https://download.jetbrains.com/python/pycharm-community-2020.3.tar.gz
+tar -xf pycharm-community-2020.3.tar.gz
+cd pycharm-community-2020.3/
+cd bin/
+./pycharm.sh
+echo For getting the pycharm-community IDE on your desktop follow the steps below:
+echo 1. Click on configure
+echo 2. Click on "Create Desktop Entry"
+echo Now you'll be able to access pycharm from your desktop.
+rm pycharm-community-2020-3
+rm -rf pycharm-community-2020-3.tar.gz
