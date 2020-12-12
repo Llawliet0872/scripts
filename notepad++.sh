79c8a004acda94caf9fbd3407597f9b50c2be753
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 11:05:31 +0530

    Scrits for litepack

--- notepad++.sh
+++ notepad++.sh
@@ -1,6 +1,17 @@
 wget https://github.com/notepad-plus-plus/notepad-plus-plus/releases/download/v7.9.1/npp.7.9.1.Installer.exe
 echo One of the required dependencies is WINE. It will be installed if it doesnt pre-exist in the system [y/n]: 
-sudo apt install wine-stable
+read option
+if [option == "y"]
+then
+	sudo apt install wine-stable
+else 
+	if [option == "n"]
+	then 
+		echo Installation Aborted!
+	
+	fi
+fi	
+	
 wine npp.7.9.1.Installer.exe
 rm npp.7.9.1.Installer.exe
 
