f98412d9664817f80b70242fcf8536608a3b0ada
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 18:51:00 +0530

    Scripts for litepack

--- eclipse.sh
+++
@@ -1,9 +0,0 @@
-echo Installing default-jre (dependency)
-sudo apt install default-jre &> /dev/null
-wget http://ftp.yz.yamagata-u.ac.jp/pub/eclipse/oomph/epp/2020-06/R/eclipse-inst-linux64.tar.gz 
-tar -xf eclipse-inst-linux64.tar.gz
-cd eclipse-installer/
-sudo ./eclipse-inst 
-cd ..
-rm -rf eclipse-installer/ 
-rm eclipse-inst-linux64.tar.gz
