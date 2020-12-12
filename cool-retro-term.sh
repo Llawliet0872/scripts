84c672e9446e41c31989a7b125b3ec3d3590e2a0
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 20:18:33 +0530

    List of packages

--- cool-retro-term.sh
+++
@@ -1,6 +0,0 @@
-echo Installing dependencies
-sudo apt-get install build-essential qml-module-qtgraphicaleffects qml-module-qt-labs-folderlistmodel qml-module-qt-labs-settings qml-module-qtquick-controls qml-module-qtquick-dialogs qmlscene qt5-default qt5-qmake qtdeclarative5-dev qtdeclarative5-localstorage-plugin qtdeclarative5-qtquick2-plugin qtdeclarative5-window-plugin
-git clone https://github.com/Swordfish90/cool-retro-term.git
-cd cool-retro-term
-qmake && make
-./cool-retro-term
