a493f497845f30d617eac913f46f5ec5f7e1d617
Llawliet0872 <arnav0872@gmail.com>
2020-12-12 14:30:08 +0530

    Scripts for litepack

--- postman-64-bit.sh
+++ postman-64-bit.sh
@@ -2,4 +2,5 @@ wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
 sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
-
+echo > "[Desktop Entry]<br>Encoding=UTF-8<br>Name=Postman<br>Exec=/opt/Postman/app/Postman %U<br>Icon=/opt/Postman/app/resources/app/assets/icon.png<br>Terminal=false<br>Type=Application<br>Categories=Development;" ~/.local/share/applications/Postman.desktop
+rm -rf Postman
