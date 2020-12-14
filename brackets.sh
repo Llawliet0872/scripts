03b4498200f1ac0b15bbfedaa0fce18b8c9bed5c
Llawliet0872 <arnav0872@gmail.com>
2020-12-14 16:08:00 +0530

    Scripts for litepack

---
+++ brackets.sh
@@ -0,0 +1,9 @@
+echo Installing Dependencies
+wget https://launchpad.net/ubuntu/+archive/primary/+files/libgcrypt11_1.5.3-2ubuntu4.2_amd64.deb
+sudo dpkg -i libgcrypt11_1.5.3-2ubuntu4.2_amd64.deb
+rm libgcrypt11_1.5.3-2ubuntu4.2_amd64.deb
+echo Done!
+echo installing brackets
+wget wget https://github.com/adobe/brackets/releases/download/release-1.11/Brackets.Release.1.11.64-bit.deb -o brackets
+sudo dpkg -i brackets
+rm brackets
