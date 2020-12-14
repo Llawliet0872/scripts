744ef1907fdebc02252126119c3423cb8838c970
Llawliet0872 <arnav0872@gmail.com>
2020-12-14 11:55:40 +0530

    List of packages

--- brackets.sh
+++
@@ -1,7 +0,0 @@
-echo Installing Dependencies
-sudo apt-get install libgcrypt11-dev
-sudo apt install libpango1
-sudo apt install libcurl13
-wget https://github.com/adobe/brackets/releases/download/release-1.14.1/Brackets.Release.1.14.1.64-bit.deb -O brackets
-sudo dpkg -i brackets
-rm brackets
