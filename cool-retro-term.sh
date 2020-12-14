543ee01082c1c747a78acf8346449b6f59ecd4f4
Llawliet0872 <arnav0872@gmail.com>
2020-12-14 15:07:00 +0530

    Scripts for litepack

--- cool-retro-term.sh
+++ cool-retro-term.sh
@@ -1,5 +1,5 @@
 echo Installing Dependencies
 sudo apt install build-essential qmlscene qt5-qmake qt5-default qtdeclarative5-dev qml-module-qtquick-controls qml-module-qtgraphicaleffects qml-module-qtquick-dialogs qml-module-qtquick-localstorage qml-module-qtquick-window2 qml-module-qt-labs-settings qml-module-qt-labs-folderlistmodel
-git clone https://github.com/Swordfish90/cool-retro-term.git
+git clone --recursive https://github.com/Swordfish90/cool-retro-term.git
 cd cool-retro-term
 qmake;make
