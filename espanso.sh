cf95afdf435bee8c235f277ff3603551a15a00b8
Llawliet0872 <arnav0872@gmail.com>
2020-12-14 13:37:25 +0530

    Scripts for litepack

--- espanso.sh
+++ espanso.sh
@@ -1,3 +1,5 @@
+echo Installing Dependencies
+sudo apt install libxtst6 libxdo3 xclip libnotify-bin
 wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-amd64.deb
 sudo dpkg -i espanso-debian-amd64.deb
 rm espanso-debian-amd64.deb
