03393f9e11512fa4a2d6a972eef93da3b51e2d4e
Llawliet0872 <arnav0872@gmail.com>
2020-12-14 13:31:29 +0530

    Scripts for litepack

---
+++ espanso.sh
@@ -0,0 +1,4 @@
+wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-amd64.deb
+sudo dpkg -i espanso-debian-amd64.deb
+rm espanso-debian-amd64.deb
+echo Use espanso start to use it
