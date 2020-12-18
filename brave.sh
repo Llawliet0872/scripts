3d11b54332a99bfa24686521aa98b00b8f6dc4f3
Llawliet0872 <arnav0872@gmail.com>
2020-12-18 19:31:49 +0530

    Scripts for oak

--- brave.sh
+++ brave.sh
@@ -1,5 +1,3 @@
-sudo apt install apt-transport-https curl gnupg 
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc -s | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add - 
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list 
-sudo apt update 
-sudo apt install brave-browser 
+wget https://github.com/brave/brave-browser/releases/download/v1.19.64/brave-browser-dev_1.19.64_amd64.deb -o brave
+sudo dpkg -i brave
+rm brave
