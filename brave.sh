be23a96bb70c07df1dcc797dcd42772d0221b06a
Llawliet0872 <arnav0872@gmail.com>
2020-12-18 20:11:46 +0530

    Scripts for oak

--- brave.sh
+++ brave.sh
@@ -1,3 +1,3 @@
-wget https://github.com/brave/brave-browser/releases/download/v1.19.64/brave-browser-dev_1.19.64_amd64.deb -O brave
+wget https://github.com/brave/brave-browser/releases/download/v1.19.64/brave-browser_1.19.64_amd64.deb -O brave
 sudo dpkg -i brave
 rm brave
