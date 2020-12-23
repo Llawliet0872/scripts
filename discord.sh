d8a424ab9715ba316076abca3026b28ddeb1d1e0
Llawliet0872 <arnav0872@gmail.com>
2020-12-23 15:09:35 +0530

    Scripts for oak

--- discord.sh
+++ discord.sh
@@ -1,3 +1,7 @@
+echo Installing Dependencies...
+sudo apt install libgconf-2-4
+sudo apt install libappindicator1
+sudo apt install libc++1
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb" 
 sudo dpkg -i discord.deb 
-rm discord.deb 
+rm discord.deb
