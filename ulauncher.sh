b794fb8c8664cf48444fd298c920a6f902602af1
Llawliet0872 <arnav0872@gmail.com>
2020-12-25 15:14:43 +0530

    Scripts for oak

--- ulauncher.sh
+++ ulauncher.sh
@@ -1,3 +1,3 @@
-wget https://github.com/Ulauncher/Ulauncher/releases/download/5.9.0/ulauncher_5.9.0_all.deb -O ulauncher
-sudo dpkg -i ulauncher
+wget https://github.com/Ulauncher/Ulauncher/releases/download/5.9.0/ulauncher_5.9.0_all.deb
+sudo apt install ./ulauncher_5.9.0_all.deb
 rm ulauncher
