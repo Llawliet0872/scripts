ad173a4e713b13048884e1f8f04aabdabe822c13
Llawliet0872 <arnav0872@gmail.com>
2021-01-06 16:25:30 +0530

    Removed SDL2 since apt has it

--- install/SDL2.sh
+++
@@ -1,9 +0,0 @@
-wget https://www.libsdl.org/release/SDL2-2.0.14.tar.gz 
-tar -xf SDL2-2.0.14.tar.gz
-cd SDL2-2.0.14
-./configure 
-make all 
-sudo make install 
-cd ..
-rm -rf SDL2-2.0.14 
-rm SDL2-2.0.14.tar.gz 
