28db712b299c33ecddac2959edcb1cdb7abbffb4
Llawliet0872 <arnav0872@gmail.com>
2021-01-06 15:51:54 +0530

    Scripts for oak

--- install/espanso.sh
+++ install/espanso.sh
@@ -3,4 +3,4 @@ sudo apt install libxtst6 libxdo3 xclip libnotify-bin
 wget https://github.com/federico-terzi/espanso/releases/latest/download/espanso-debian-amd64.deb
 sudo dpkg -i espanso-debian-amd64.deb
 rm espanso-debian-amd64.deb
-echo Use espanso start to use it
+printf "Use \"espanso start\" to use it\n"
