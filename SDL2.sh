45a8bdf3879c5ba6fc54c1921a7227a10914a702
Llawliet0872 <arnav0872@gmail.com>
2021-01-06 13:33:15 +0530

    Updated to latest version

--- SDL2.sh
+++ SDL2.sh
@@ -1,9 +1,9 @@
-wget https://www.libsdl.org/release/SDL2-2.0.12.tar.gz 
-tar -xf SDL2-2.0.12.tar.gz
-cd SDL2-2.0.12
+wget https://www.libsdl.org/release/SDL2-2.0.14.tar.gz 
+tar -xf SDL2-2.0.14.tar.gz
+cd SDL2-2.0.14
 ./configure 
 make all 
 sudo make install 
 cd ..
-rm -rf SDL2-2.0.12 
-rm SDL2-2.0.12.tar.gz 
+rm -rf SDL2-2.0.14 
+rm SDL2-2.0.14.tar.gz 
