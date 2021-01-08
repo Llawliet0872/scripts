8641402aaed11050b62cf323b4ff5837ab2c1519
Llawliet0872 <arnav0872@gmail.com>
2021-01-08 12:08:33 +0530

    Scripts for peek

---
+++ install/peek.sh
@@ -0,0 +1,9 @@
+sudo apt install cmake valac libgtk-3-dev libkeybinder-3.0-dev libxml2-utils gettext txt2man
+git clone https://github.com/phw/peek.git
+mkdir peek/build
+cd peek/build
+cmake -DCMAKE_INSTALL_PREFIX=/usr -DGSETTINGS_COMPILE=OFF ..
+make package
+sudo apt install ./peek-*-Linux.deb
+cd ..; cd..
+rm -rf peek
